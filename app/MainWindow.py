11d0fa1c237bf3004a299ba8e7c6f08324858718
liqilei <blighli@gmail.com>
2025-11-08 02:00:19 +0800

    add SerialPort UI and Logic

--- app/MainWindow.py
+++ app/MainWindow.py
@@ -1,14 +1,77 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QLabel, QComboBox, QMessageBox
 from PyQt6.QtGui import QIcon
-from .utils import assets_path
+from PyQt6.QtSerialPort import QSerialPort, QSerialPortInfo
+from .utils import assets_path, serial_port
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.port: QSerialPort | None = None
         self.initUI()
 
 
     def initUI(self):
+
+        # Main Window Settings
         self.setWindowIcon(QIcon(assets_path.get('assets//app.ico')))
         self.setWindowTitle("Hello from HardCode!")
         self.setGeometry(100, 100, 800, 600)
+
+        # Central Widget and Layouts
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        # Create Main Layouts
+        mainBox = QHBoxLayout(central_widget)
+        leftBox = QVBoxLayout()
+        rightBox = QVBoxLayout()
+        mainBox.addLayout(leftBox)
+        mainBox.addLayout(rightBox)
+
+
+        # Serial Port Selection
+        portBox = QHBoxLayout()
+        leftBox.addLayout(portBox)
+        leftBox.addStretch()
+        # Serial Port ComboBox
+        self.portComboBox = QComboBox()
+        self.portComboBox.setFixedWidth(200)
+        portList = serial_port.get_port_names()
+        for port in portList:
+            self.portComboBox.addItem(port)
+        # Baud Rate ComboBox
+        self.baudRateComboBox = QComboBox()
+        self.baudRateComboBox.setFixedWidth(100)
+        self.baudRateComboBox.addItems(["9600", "19200", "38400", "57600", "115200", "230400", "460800", "921600"])
+        self.baudRateComboBox.setCurrentIndex(4)  # Default to 115200
+        # Connect Button
+        self.portConnectButton = QPushButton("打开串口")
+        self.portConnectButton.setFixedWidth(100)
+        # Add widgets to portBox
+        portBox.addWidget(self.portComboBox)
+        portBox.addWidget(self.baudRateComboBox)
+        portBox.addWidget(self.portConnectButton)
+
+        # Connect Button Signal
+        self.portConnectButton.clicked.connect(self.connectPort)
+
+    def connectPort(self):
+        # Serial Port Connection Logic
+        if self.port is not None:
+            self.port.close()
+            self.port = None
+            self.statusBar().showMessage("Serial port closed.")
+            self.portComboBox.setEnabled(True)
+            self.baudRateComboBox.setEnabled(True)
+            self.portConnectButton.setText("打开串口")
+            return
+        # Connect to Serial Port
+        port = self.portComboBox.currentText()
+        baud_rate = int(self.baudRateComboBox.currentText())
+        self.port = serial_port.connect(port, baud_rate)
+        if self.port is not None:
+            self.statusBar().showMessage(f"Connected to {port} at {baud_rate} baud.")
+            self.portComboBox.setEnabled(False)
+            self.baudRateComboBox.setEnabled(False)
+            self.portConnectButton.setText("关闭串口")
+        else:
+            QMessageBox.critical(self, "Connection Error", f"Failed to connect to {port} at {baud_rate} baud.") 
---
+++ app/utils/serial_port.py
@@ -0,0 +1,23 @@
+from PyQt6.QtSerialPort import QSerialPort, QSerialPortInfo
+
+def get_port_names():
+    portList = QSerialPortInfo.availablePorts()
+    portList = sorted(portList, key=lambda port: eval(port.portName()[3:]))
+    return [port.portName() for port in portList]
+
+def get_port_by_name(name: str) -> QSerialPort | None:
+    portList = QSerialPortInfo.availablePorts()
+    for portInfo in portList:
+        if portInfo.portName() == name:
+            serialPort = QSerialPort(portInfo)
+            return serialPort
+    return None
+
+def connect(port_name: str, baud_rate: int) -> QSerialPort | None:
+    serialPort = get_port_by_name(port_name)
+    if serialPort is None:
+        return None
+    serialPort.setBaudRate(baud_rate)
+    if not serialPort.open(QSerialPort.OpenModeFlag.ReadWrite):
+        return None
+    return serialPort
