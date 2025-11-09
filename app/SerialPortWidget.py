f2adb798d817dfe850d0aae5b2ba967e857c763e
liqilei <blighli@gmail.com>
2025-11-10 02:53:37 +0800

    fix port name display in SerialPortWidget when failed MessageBox shows

--- app/SerialPortWidget.py
+++ app/SerialPortWidget.py
@@ -71,4 +71,4 @@ class SerialPortWidget(QWidget):
             self.portConnectButton.setText("关闭串口")
             self.port_open.emit(self.serialPort)
         else:
-            QMessageBox.critical(self, "Connection Error", f"Failed to connect to {port} at {baud_rate} baud.") 
+            QMessageBox.critical(self, "Connection Error", f"Failed to connect to {port.portName()} at {baud_rate} baud.")
