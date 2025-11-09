9708860d46e8aec73fae5236054ccba7de4fd717
liqilei <blighli@gmail.com>
2025-11-10 00:36:27 +0800

    redirect fastapi log output to dummyfile in exe without tty terminal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QApplication
 from app.MainWindow import MainWindow
 
+import sys, os
+
 def main():
     app = QApplication([])
     window = MainWindow()
@@ -8,4 +10,15 @@ def main():
     app.exec()
 
 if __name__ == "__main__":
+
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+      # Running in a PyInstaller bundle
+      class DummyFile:
+          def write(self, x): pass
+          def flush(self): pass
+          def isatty(self): return False # Indicate not a TTY
+
+      sys.stdout = DummyFile()
+      sys.stderr = DummyFile()
+    
     main()
