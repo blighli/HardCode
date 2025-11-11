6eb4ee4eb52c99d29bddb1ef66f849fca7bff3d5
liqilei <blighli@gmail.com>
2025-11-11 14:11:37 +0800

    disable old school opengl code in GraphicsWidget to make linux run

--- app/GraphicsWidget.py
+++ app/GraphicsWidget.py
@@ -8,10 +8,10 @@ class GraphicsWidget(QOpenGLWidget):
     def initializeGL(self):
         # Initialize OpenGL settings here
         glEnable(GL_DEPTH_TEST)
-        glEnable(GL_LIGHT0)
-        glEnable(GL_LIGHTING)
-        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
-        glEnable(GL_COLOR_MATERIAL)
+        #glEnable(GL_LIGHT0)
+        #glEnable(GL_LIGHTING)
+        #lorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        #glEnable(GL_COLOR_MATERIAL)
         glClearColor(1.0, 1.0, 1.0, 1.0)  # Set clear color to white
 
         # Get OpenGL version information
@@ -33,12 +33,12 @@ class GraphicsWidget(QOpenGLWidget):
     def paintGL(self):
         # Render OpenGL content here
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glBegin(GL_TRIANGLES)
-        glColor3f(1.0, 0.0, 0.0)  # 红色
-        glVertex3f(-0.5, -0.5, 0.0)
-        glColor3f(0.0, 1.0, 0.0)  # 绿色
-        glVertex3f(0.5, -0.5, 0.0)
-        glColor3f(0.0, 0.0, 1.0)  # 蓝色
-        glVertex3f(0.0, 0.5, 0.0)
-        glEnd()
+        # glBegin(GL_TRIANGLES)
+        # glColor3f(1.0, 0.0, 0.0)  # 红色
+        # glVertex3f(-0.5, -0.5, 0.0)
+        # glColor3f(0.0, 1.0, 0.0)  # 绿色
+        # glVertex3f(0.5, -0.5, 0.0)
+        # glColor3f(0.0, 0.0, 1.0)  # 蓝色
+        # glVertex3f(0.0, 0.5, 0.0)
+        # glEnd()
 
