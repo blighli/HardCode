e53617048fe084feabbf158d44bc7520a489117a
liqilei <blighli@gmail.com>
2025-11-27 01:57:51 +0800

    add Vision Widget

---
+++ app/VisionWidget.py
@@ -0,0 +1,11 @@
+class VisionWidget:
+    def __init__(self):
+        pass
+
+    def display_feed(self):
+        # Code to display the feed in the widget
+        print("Displaying vision feed:", feed)
+
+    def capture_image(self):
+        # Code to handle the captured image
+        print("Captured image:", image)
