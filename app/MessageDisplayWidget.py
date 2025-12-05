d569f4b8ce8bc811f4c55e4075c9239c637fa149
liqilei <blighli@gmail.com>
2025-12-06 02:28:06 +0800

    add clear and isHex context menu to MessageDisplayWidget

--- app/MessageDisplayWidget.py
+++ app/MessageDisplayWidget.py
@@ -1,10 +1,12 @@
 from PyQt6.QtWidgets import QWidget, QTextEdit, QHBoxLayout, QLineEdit, QPushButton, QCheckBox
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import Qt, pyqtSignal
 
 class MessageDisplayWidget(QWidget):
 
     def __init__(self, parent=None):
         super(MessageDisplayWidget, self).__init__(parent)
+
+        self.hexMode = False
 
         layout = QHBoxLayout()
         self.setLayout(layout)
@@ -13,6 +15,21 @@ class MessageDisplayWidget(QWidget):
         self.messageDisplay.setReadOnly(True)
         self.messageDisplay.setStyleSheet("font-family: Consolas;")
         layout.addWidget(self.messageDisplay)
+        
+        self.messageDisplay.addAction("Hex Mode", self.toggleHexMode).setCheckable(True)
+        self.messageDisplay.setContextMenuPolicy(Qt.ContextMenuPolicy.ActionsContextMenu)
+
+        self.messageDisplay.addAction("Clear", self.clearMessages)
+
+    def isHexMode(self):
+        return self.hexMode
+
+    def clearMessages(self):
+        self.messageDisplay.clear()
+
+    def toggleHexMode(self):
+        self.hexMode = not self.hexMode
+        print(f"Hex Mode {'Enabled' if self.hexMode else 'Disabled'}")
 
     def appendMessage(self, message):
         self.messageDisplay.setPlainText(self.messageDisplay.toPlainText() + message)
